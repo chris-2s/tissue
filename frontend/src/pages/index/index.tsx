1dafe1f7fdf3cebf47396f7b48fadf25d55dd4bc
chris-2s <159798260+chris-2s@users.noreply.github.com>
2025-01-29 22:25:38 +0800

    手机端界面适配加强

--- frontend/src/pages/index/index.tsx
+++ frontend/src/pages/index/index.tsx
@@ -2,18 +2,18 @@ import {Layout, theme, Drawer, ConfigProvider, FloatButton} from "antd";
 import {Navigate, Outlet} from "react-router-dom";
 import Styles from "./index.module.css";
 import React, {useEffect, useRef, useState} from "react";
-import {useScreen} from "../../utils/useScreen";
 import Sider from "./sider";
 import Header from "./header";
 import {useDispatch, useSelector} from "react-redux";
 import {Dispatch, RootState} from "../../models";
+import {useResponsive} from "ahooks";
+import TabBar from "./tabBar.tsx";
 
 const {useToken} = theme
 
 function Index() {
 
-    const size = useScreen()
-    const isLg = size.width >= 992
+    const responsive = useResponsive()
 
     const [collapsed, setCollapsed] = useState(true)
     const {token} = useToken()
@@ -49,20 +49,22 @@ function Index() {
                 }
             }}>
                 <Layout style={{height: '100%'}}>
-                    {isLg ? (
+                    {responsive.lg ? (
                         <Layout.Sider
                             style={{background: token.colorBgLayout, borderRightColor: token.colorBorderSecondary}}
                             className={`${Styles.side} overflow-y-auto pt-2`}>
                             <Sider showLogo={false}/>
                         </Layout.Sider>
                     ) : (
-                        <Drawer style={{
-                            width: 'calc(100% + env(safe-area-inset-left))',
-                            paddingLeft: 'env(safe-area-inset-left)'
-                        }} width={208} title={null} closeIcon={null} placement={'left'}
-                                open={!collapsed} onClose={() => setCollapsed(true)} styles={{body: {padding: 0}}}>
-                            <Sider onSelect={() => setCollapsed(true)}/>
-                        </Drawer>
+                        responsive.md && (
+                            <Drawer style={{
+                                width: 'calc(100% + env(safe-area-inset-left, 0))',
+                                paddingLeft: 'env(safe-area-inset-left, 0)'
+                            }} width={208} title={null} closeIcon={null} placement={'left'}
+                                    open={!collapsed} onClose={() => setCollapsed(true)} styles={{body: {padding: 0}}}>
+                                <Sider onSelect={() => setCollapsed(true)}/>
+                            </Drawer>
+                        )
                     )}
                     <Layout style={{position: 'relative'}}>
                         <div className={Styles.header} style={{
@@ -70,12 +72,17 @@ function Index() {
                             borderBlockEndColor: token.colorBorderSecondary
                         }}>
                             <Layout.Header className={'bg-transparent'}>
-                                <Header collapsible={!isLg} onCollapse={() => setCollapsed(!collapsed)}/>
+                                <Header collapsible={responsive.md && !responsive.lg}
+                                        onCollapse={() => setCollapsed(!collapsed)}/>
                             </Layout.Header>
                         </div>
-                        <Layout.Content style={{overflowY: "auto"}}
-                                        className={Styles.content}>
-                            <div style={{padding: isLg ? 16 : 12}}>
+                        <Layout.Content
+                            style={{
+                                overflowY: "auto",
+                                paddingBottom: (!responsive.md && !responsive.lg) ? ('calc(50px + env(safe-area-inset-bottom, 0))') : 0
+                            }}
+                            className={Styles.content}>
+                            <div style={{padding: responsive.md ? 16 : 12}}>
                                 <Outlet/>
                                 <FloatButton.Group className={'index-float-button-group'}>
                                     <FloatButton.BackTop/>
@@ -83,6 +90,14 @@ function Index() {
                             </div>
                         </Layout.Content>
                     </Layout>
+                    {(!responsive.md && !responsive.lg) && (
+                        <div className={Styles.footer} style={{
+                            background: token.colorBgContainer + '99',
+                            borderBlockStartColor: token.colorBorderSecondary
+                        }}>
+                            <TabBar/>
+                        </div>
+                    )}
                 </Layout>
             </ConfigProvider>
         </div>
