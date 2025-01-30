8952aa9e841c05d51db0c6793c57daa3f11505d9
chris-2s <159798260+chris-2s@users.noreply.github.com>
2025-01-30 09:45:32 +0800

    添加图片懒加载

--- frontend/src/configs/development.ts
+++ frontend/src/configs/development.ts
@@ -1,7 +1,7 @@
 import {ConfigProperties} from "./index";
 
 const config: ConfigProperties = {
-    BASE_API: "http://localhost:8000",
+    BASE_API: "http://192.168.50.116:8000",
 }
 
 export default config
