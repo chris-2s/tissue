b973021e960ed299e3a4bb7ef8bd40f8d2587543
chris-2s <436071849@qq.com>
2025-03-30 18:17:48 +0800

    解决预览视频失败问题

--- app/api/common.py
+++ app/api/common.py
@@ -32,7 +32,7 @@ async def proxy_video_trailer(url: str, request: Request):
     }
 
     async with httpx.AsyncClient() as client:
-        if url.strip("//"):
+        if url.startswith("//"):
             url = 'http:' + url
         response = await client.get(url, headers=headers)
         response.raise_for_status()
