059b7d15ee8f172abc059e9dfe0bf5d871abb382
chris-2s <436071849@qq.com>
2025-04-04 12:15:19 +0800

    首屏启动优化

--- frontend/src/models/app.ts
+++ frontend/src/models/app.ts
@@ -1,23 +1,22 @@
 import {createModel} from "@rematch/core";
 import {RootModel} from "./index";
-import Cookies from 'js-cookie';
 import {themes} from "../utils/constants";
 
 
 export const app = createModel<RootModel>()({
     state: {
-        themeMode: Cookies.get('themeMode') || themes[0].name,
-        goodBoy: Cookies.get('goodBoy') == '1' || false,
+        themeMode: localStorage.getItem('themeMode') || themes[0].name,
+        goodBoy: localStorage.getItem('goodBoy') == '1' || false,
         canBack: false,
         pin: localStorage.getItem('pin'),
     },
     reducers: {
         setThemeMode(state, payload: string) {
-            Cookies.set('themeMode', payload, {expires: 365})
+            localStorage.setItem('themeMode', payload)
             return {...state, themeMode: payload}
         },
         setGoodBoy(state, payload: boolean) {
-            Cookies.set('goodBoy', payload ? '1' : '0', {expires: 365})
+            localStorage.setItem('goodBoy', payload ? '1' : '0')
             return {...state, goodBoy: payload}
         },
         setCanBack(state, payload: boolean) {
