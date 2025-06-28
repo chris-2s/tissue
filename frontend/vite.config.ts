eee983833c59b224cc314e77bf1b3182a1365933
chris-2s <436071849@qq.com>
2025-06-28 14:05:33 +0800

    样式调整

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,9 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import {TanStackRouterVite} from '@tanstack/router-plugin/vite'
+import {tanstackRouter} from '@tanstack/router-plugin/vite'
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [TanStackRouterVite({target: 'react', autoCodeSplitting: false}), tailwindcss(), react()],
+    plugins: [tanstackRouter({target: 'react', autoCodeSplitting: false}), tailwindcss(), react()],
 })
