db8c8cc3c760c6a7bfa88e2ff8002193e464643e
chris-2s <159798260+chris-2s@users.noreply.github.com>
2026-01-18 19:01:56 +0800

    添加另一个站点的演员页面，解决下载任务分类设置实效问题

--- frontend/src/routes/_index/search/-components/actorsModal.tsx
+++ frontend/src/routes/_index/search/-components/actorsModal.tsx
@@ -1,6 +1,5 @@
-import {SearchOutlined} from "@ant-design/icons";
-import {Modal, ModalProps, Segmented, Space} from "antd";
-import {useEffect, useState} from "react";
+import {Modal, ModalProps, Space, Tag} from "antd";
+import {useNavigate} from "@tanstack/react-router";
 
 interface Props extends ModalProps {
     actors: any
@@ -9,35 +8,28 @@ interface Props extends ModalProps {
 function ActorsModal(props: Props) {
 
     const {actors, ...otherProps} = props
-    const [selectedSite, setSelectedSite] = useState<string>()
-
-    const sites = actors.map((item: any) => item.website)
-
-    useEffect(() => {
-        if (otherProps.open) setSelectedSite(sites[0])
-    }, [otherProps.open])
-
-    const items = actors.find((item: any) => item.website === selectedSite)?.items
+    const navigate = useNavigate()
 
     return (
         <Modal {...otherProps} title={'演员'} footer={null} centered>
-            <div className={'text-center'}>
-                <Segmented options={sites}
-                           value={selectedSite}
-                           onChange={value => {
-                               setSelectedSite(value)
-                           }}/>
-            </div>
-            {items && (
-                <Space wrap className={'mt-2'}>
-                    {items.map((item: any) => (
-                        <span>
-                            <SearchOutlined/>
-                            <a>{item.name}</a>
-                        </span>
-                    ))}
-                </Space>
-            )}
+            <Space direction="vertical">
+                {actors?.map((site: any) => (
+                    <div>
+                        <div className={'font-extrabold mb-2'}>{site.website}</div>
+                        <Space wrap={true} size={[5, 10]}>
+                            {site.items.map((actor: any) => (
+                                <Tag className={'cursor-pointer'}>
+                                    <a onClick={() => {
+                                        navigate({
+                                            to: '/actor', search: {source: site.website, code: actor.code}
+                                        })
+                                    }}>{actor.name}</a>
+                                </Tag>
+                            ))}
+                        </Space>
+                    </div>
+                ))}
+            </Space>
         </Modal>
     )
 }
