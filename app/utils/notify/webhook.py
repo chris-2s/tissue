6cfe96b50b8b06ed2e0ba43b980113e0f2d39bf1
chris-2s <436071849@qq.com>
2026-02-19 10:46:29 +0800

    添加cookie失效通知

--- app/utils/notify/webhook.py
+++ app/utils/notify/webhook.py
@@ -1,6 +1,6 @@
 import requests
 
-from app.schema import VideoNotify, SubscribeNotify
+from app.schema import VideoNotify, SubscribeNotify, CookieNotify
 from app.utils.notify.base import Base
 
 
@@ -11,6 +11,9 @@ class Webhook(Base):
 
     def send_subscribe(self, subscribe: SubscribeNotify):
         pass
+
+    def send_cookie(self, cookie: CookieNotify):
+        self.send('cookie', cookie.model_dump())
 
     def send(self, event: str, payload: dict):
         requests.post(self.setting.webhook_url, json={
